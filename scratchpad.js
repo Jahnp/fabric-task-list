291c5537fe6d0f0184a8c12f2c6e54e555462270
Peter Jahn <pejahn@microsoft.com>
2018-09-18 10:34:32 -0700

    Update scratchpad

--- scratchpad.js
+++ scratchpad.js
@@ -1,12 +1,18 @@
-// Import FluentCustomizations
-import { FluentCustomizations } from '@uifabric/experiments/lib/components/fluent/FluentCustomizations';
+// Code snippets for adding a Fabric React component and applying Fluent styles
 
-// Customizer
-<Customizer {...FluentCustomizations}>
-</Customizer>
-
-// ProgressIndicator
+// Step 1: Import and add add ProgressIndicator
 <ProgressIndicator
   label='Your teams progress'
   description={`${this._TaskManager.getCompletedTaskCount()} of ${this._TaskManager.getTaskCount()} tasks completed`}
   percentComplete={this._TaskManager.getTasksPercentComplete()} />
+
+// Step 2: Import FluentCustomizations
+import { FluentCustomizations } from '@uifabric/experiments/lib/components/fluent/FluentCustomizations';
+
+// Step 3: Wrap app in <Customizer> and pass FluentCustomizations
+<Customizer {...FluentCustomizations}>
+  <App />
+</Customizer>
+
+// Step 4: Open App.scss to make Fabric Core changes.
+

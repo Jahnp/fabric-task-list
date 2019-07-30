7ead273c47acb73289deb3c530f46f0ca4d1fbdc
Peter Jahn <pejahn@microsoft.com>
2019-07-30 15:19:29 -0700

    Update sidenav and add progress indicator import snippet

--- src/Sidenav.tsx
+++ src/Sidenav.tsx
@@ -1,5 +1,46 @@
 import * as React from "react";
+import { getTheme } from "office-ui-fabric-react";
+import { Sidebar } from "@uifabric/experiments";
 
 export const Sidenav = (props: any) => {
-  return <div className="Sidenav-placeholder">[Sidenav control goes here]</div>;
+  return (
+    <Sidebar
+      collapsible={true}
+      theme={getTheme()}
+      collapseButtonAriaLabel={"sitemap"}
+      items={[
+        {
+          key: "collapsible-example-item1",
+          name: "My Day",
+          iconProps: { iconName: "Sunny" },
+          active: true
+        },
+        {
+          key: "collapsible-example-item2",
+          name: "Important",
+          iconProps: { iconName: "FavoriteStar" },
+          active: false
+        },
+        {
+          key: "collapsible-example-item3",
+          name: "Tasks",
+          iconProps: { iconName: "Home" },
+          active: false
+        },
+        {
+          key: "collapsible-example-item4",
+          name: "Lists",
+          iconProps: { iconName: "BulletedList" },
+          active: false
+        }
+      ]}
+      footerItems={[
+        {
+          key: "footer-settings",
+          name: "Settings",
+          iconProps: { iconName: "Settings" }
+        }
+      ]}
+    />
+  );
 };
